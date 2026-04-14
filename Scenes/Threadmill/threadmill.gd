3f325da3276b2dd16f530f675abc4e43724ae898
Jeremy_Leung <Jeremy.Leung48@gmail.com>
2026-04-14 14:20:46 -0400

    Levels 1 and 2 First Pass
    Created basic layout for levels 1 and 2.
    Created a second scene for level 2.
    Added more tiles from test set onto collision layer.
    Adjusted values for the treadmill for testing.

--- Scenes/Threadmill/threadmill.gd
+++ Scenes/Threadmill/threadmill.gd
@@ -1,14 +1,14 @@
 # represents a threadmill block, it will move whatever is mark as Pushable
 extends Area2D
 
-@export var impulse: float = 5.0
+@export var impulse: float = 1.0
 @export var move_speed: float = 100.0
 
 var elements_to_push: Array[Node2D] = []
 
 func _physics_process(delta: float) -> void:
 	for body in elements_to_push:
-		body.velocity.x = lerp(body.velocity.x, 1 * move_speed, impulse * delta)
+		body.velocity.x = lerp(body.velocity.x, 5 * move_speed, impulse * delta)
 
 
 func _on_body_entered(body: Node2D) -> void:
